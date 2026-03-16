7a36af32881fbaed3ebdad0b0d8e9f81cee507af
Aryan kumar <aryan002005@gmail.com>
2026-03-16 23:58:55 +0530

    use of math/special function

---
+++ math-special.js
@@ -0,0 +1,11 @@
+const sqrt = require('@stdlib/math/base/special/sqrt');
+const abs = require('@stdlib/math/base/special/abs');
+const factorial = require('@stdlib/math/base/special/factorial');
+const ceil = require('@stdlib/math/base/special/ceil');
+const floor = require('@stdlib/math/base/special/floor');
+
+console.log('Square root of 144:', sqrt(144));
+console.log('Absolute value of -42:', abs(-42));
+console.log('Factorial of 6:', factorial(6));
+console.log('Ceil of 4.3:', ceil(4.3));
+console.log('Floor of 4.9:', floor(4.9));
